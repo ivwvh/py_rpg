52eb11ef3207ab060745afa282941aa52159c247
ivwvh <68565784+ivwvh@users.noreply.github.com>
2022-12-23 12:21:34 +0300

    Update equiptest.py

--- equiptest.py
+++ equiptest.py
@@ -18,7 +18,7 @@ def show_inventory(hero:list)->None:
         print(*item)
 
 
-def equip_o_matic3000(otkuda:list, kuda:list)->None:
+def equip_item(otkuda:list, kuda:list)->None:
     show_inventory(list_1)
     idx = int(input("Какой предмет экипировать?: "))
     kuda.append(otkuda[idx])
